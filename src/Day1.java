d6281e6e79a7f2491b6c00efad1288a1d54f4604
Dylan Swarts <dswarts11@gmail.com>
2022-12-07 09:43:47 +0200

    complete Day1 both

--- src/Day1.java
+++ src/Day1.java
@@ -1,14 +1,46 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Day1 {
-    public static void main(String[] args) throws Exception {
-        //read file
+    public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/Input.txt");
-        Scanner myReader =  new Scanner(file);
+        Scanner myReader = new Scanner(file);
         
+        try {
+            System.out.println("Max Cals");
+            getMaxCalories(myReader);
+            System.out.println("Total Top 3 Cals");
+            getTop3Calories();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void getMaxCalories(Scanner myReader) throws Exception {
+         //read file
+         int total = 0;
+         int largest = 0;
+         myReader.reset();
+         while (myReader.hasNextLine()) {
+             String line = myReader.nextLine();
+             if (!line.equals("")) {
+                 //is NOT blank line
+                 total+= Integer.valueOf(line);
+ 
+             } else {//reset var
+                 if (total>largest) largest = total;
+                 total = 0;
+             }
+         }
+         System.out.println(largest);    
+    }
+
+    static void getTop3Calories() throws Exception {
         int total = 0;
-        int largest = 0;
+        int largest[] = {0,0,0};
+        File file = new File("src/Input.txt");
+        Scanner myReader = new Scanner(file);
         while (myReader.hasNextLine()) {
             String line = myReader.nextLine();
             if (!line.equals("")) {
@@ -16,11 +48,22 @@ public class Day1 {
                 total+= Integer.valueOf(line);
 
             } else {//reset var
-                if (total>largest) largest = total;
+                for (int i = 0; i < largest.length; i++) {
+                    if (total>largest[i]) {
+                        for (int j = largest.length-1; j < i; j--) {
+                            largest[j] = largest[j-1]; 
+                        }
+                        largest[i] = total;
+                        break;
+                    }
+                }
                 total = 0;
             }
         }
-        System.out.println(largest);    
+        int sum = 0;
+        for (int i = 0; i < largest.length; i++) {
+            sum+=largest[i];
+        }
+        System.out.println(sum);             
     }
-
 }
